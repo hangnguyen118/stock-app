4d1608ceb9b8519cb57ff9fef715c59934ef180b
hangnguyen118 <hangnguyen118.wy@gmail.com>
2024-09-11 12:09:10 +0700

    update the migration users

---
+++ database/migrations/2024_09_11_044423_create_users_permission_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users_permission', function (Blueprint $table) {
+            $table->foreignId('users_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('permissions_id')->constrained('permissions')->onDelete('cascade');
+            $table->primary(['users_id', 'permissions_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users_permission');
+    }
+};
